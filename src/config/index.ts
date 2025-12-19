33c584eb44e4a46bffee5d14f53e63a6bbab641e
Your Name01~git <saifsahai2002@gmail.com>
2025-12-19 09:23:14 +0600

    set the connection string from database

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,7 @@ import path from "path";
 dotenv.config({ path: path.join(process.cwd(), ".env") })
 const config = {
     port: process.env.port,
+    connectionString: process.env.CONNECTION_STR
 }
 
 export default config;
